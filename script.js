e5c0f49014459ee7ca8862612a5500314e8d731f
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-13 16:53:24 -0300

    Adicionada tag p e conteudo no paragrafo

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ bodyContent.appendChild(headerTag);
 const h1Tag = document.createElement('h1');
 bodyContent.children[1].appendChild(h1Tag);
 h1Tag.innerText = 'Minha Lista de Tarefas!';
+
+const sectionParagraph = document.createElement('section');
+bodyContent.appendChild(sectionParagraph);
+
+const paragraphTag = document.createElement('p');
+bodyContent.children[2].appendChild(paragraphTag);
+paragraphTag.id = 'funcionamento';
+paragraphTag.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
