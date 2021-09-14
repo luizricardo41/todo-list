04540156d11f09b6f08e4c438e9ef6f1764276d5
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-14 15:05:28 -0300

    Adicionado para salvar informações ao fechar ou atualizar página

--- script.js
+++ script.js
@@ -47,6 +47,12 @@ buttonClearComplete.id = 'remover-finalizados';
 buttonClearComplete.innerText = 'Remover tarefas finalizadas';
 buttonClearComplete.style.marginLeft = '20px';
 
+const buttonSaveTasks = document.createElement('button');
+bodyContent.children[4].appendChild(buttonSaveTasks);
+buttonSaveTasks.id = 'salvar-tarefas';
+buttonSaveTasks.innerText = 'Salvar tarefas';
+buttonSaveTasks.style.marginLeft = '20px';
+
 function selectedTask(event) {
   const eventTargetSelect = event.target;
   const checkStyle = document.querySelectorAll('li');
@@ -87,6 +93,7 @@ function createNewTask() {
 document.getElementById('criar-tarefa').onclick = function add() { createNewTask(); };
 
 function deleteList() {
+  localStorage.clear();
   const checkListContent = document.querySelectorAll('li');
   for (let i = 0; i < checkListContent.length; i += 1) {
     checkListContent[i].parentNode.removeChild(checkListContent[i]);
@@ -94,11 +101,59 @@ function deleteList() {
 }
 document.getElementById('apaga-tudo').onclick = function clear() { deleteList(); };
 
+function removeTaskCompleteLocalStorage() {
+  const items = localStorage.getItem('items');
+  for (let i = 0; i < items; i += 1) {
+    if (localStorage.getItem(`class${i}`) === 'completed') {
+      localStorage.removeItem(`Task${i}`);
+      localStorage.removeItem(`style${i}`);
+      localStorage.removeItem(`class${i}`);
+      localStorage.removeItem('items');
+      const newList = document.querySelectorAll('li').length;
+      localStorage.setItem('items', newList);
+    }
+  }
+}
+
 function removeTaskComplete() {
   const checkTaskComplete = document.querySelectorAll('.completed');
   for (let i = 0; i < checkTaskComplete.length; i += 1) {
     checkTaskComplete[i].parentNode.removeChild(checkTaskComplete[i]);
+    removeTaskCompleteLocalStorage();
   }
 }
 const clearTaskComplete = document.getElementById('remover-finalizados');
 clearTaskComplete.onclick = function clearComplete() { removeTaskComplete(); };
+
+function saveTasks() {
+  const saveList = document.querySelectorAll('li');
+  for (let i = 0; i < saveList.length; i += 1) {
+    const keyList = `Task${i}`;
+    const styleText = `style${i}`;
+    const classitem = `class${i}`;
+    localStorage.setItem(keyList, saveList[i].innerText);
+    localStorage.setItem(styleText, saveList[i].style.textDecoration);
+    localStorage.setItem(classitem, saveList[i].classList.value);
+    localStorage.setItem('items', saveList.length);
+  }
+}
+document.getElementById('salvar-tarefas').onclick = function save() { saveTasks(); };
+
+function pullSavedData() {
+  const items = localStorage.getItem('items');
+  if (localStorage.getItem('Task0') !== '') {
+    for (let i = 0; i < items; i += 1) {
+      const createList = document.querySelector('#lista-tarefas');
+      const liTag = document.createElement('li');
+      createList.appendChild(liTag);
+      liTag.innerText = localStorage.getItem(`Task${i}`);
+      liTag.style.textDecoration = localStorage.getItem(`style${i}`);
+      liTag.className = localStorage.getItem(`class${i}`);
+      addClickList();
+    }
+  }
+}
+
+window.onload = function loadPage() {
+  pullSavedData();
+};
